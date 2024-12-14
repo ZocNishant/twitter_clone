bef9faadae3d0e3b8b3af39691167465382c9206
Nishant Zoc <zocnishant@gmail.com>
2024-12-14 18:58:58 +0545

    .

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Server is ready.");
+  res.send("Server is up and ready.");
 });
 
 app.listen(9000, () => {
